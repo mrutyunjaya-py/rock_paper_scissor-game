302968e0f7dae2cdf642c9c39bb0a6b75f77b90d
MRUTYUNJAYA RATH <143149920+mrutyunjaya-py@users.noreply.github.com>
2023-09-02 11:08:57 +0530

    Add files via upload

---
+++ rock paper scissor.py
@@ -0,0 +1,21 @@
+
+import random
+a = ["rock","paper","scissor"]
+b = None
+c = random.choice(a)
+
+while b not in a:
+    b= input("select among [rock , paper , scissor]: \n")
+user = print(f"you choose {b}")
+computer = print(f"computer choose {c}")
+
+if b == c:
+    print("tie ! ")
+elif b == "rock" and c == "scissor":
+    print("you wins!! ")
+elif b == "paper" and c == "rock":
+    print("you wins!! ")
+elif b == "scissor" and c == "paper":
+    print("you wins!! ")
+else:
+    print("you loss !!")
